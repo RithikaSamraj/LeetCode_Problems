75b97b4ef99d4d610b164dcd8ea301f1ed4dcbac
RithikaSamraj <147656825+RithikaSamraj@users.noreply.github.com>
2025-08-08 21:15:54 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (98.92%) - LeetSync

---
+++ 66-plus-one/plus-one.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n=digits.length;
+        int i=n-1;
+        while(i>=0){
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i]=0;
+            i--;
+        }
+        
+        int[] intArray = new int[n+1];
+        intArray[0]=1;
+        return intArray;
+    }
+}
